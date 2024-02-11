a4083c6ac76bdb1e0c1036e6ac5a09c1829c63e3
msa <mdsohailahmed117@gmail.com>
2024-02-11 23:09:17 +0400

    React project Start with Basic

---
+++ App.js
@@ -0,0 +1,6 @@
+
+const heading= React.createElement("h1",{id:"heading"},"new React World")
+ const root = ReactDOM.createRoot(document.getElementById("root"))
+
+ root.render(heading)
+ console.log(root)
