b72e4f9b7036f03b7d53c03d1ba229b575195419
msa <mdsohailahmed117@gmail.com>
2024-07-29 03:01:44 +0400

    optimize code in app js file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
-import React from "react";
+import React, { Suspense, lazy, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
+import { RouterProvider, createBrowserRouter, Outlet,  useLocation } from "react-router-dom";
+import { Provider } from "react-redux";
+import { CookiesProvider } from "react-cookie";
 import Body from "./components/Body";
-import { RouterProvider, createBrowserRouter,Outlet } from "react-router-dom";
+import Header from "./components/Header";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import Card from "./components/Card";
+import NavSearchFacets from "./components/NavSearchFacets";
+import FilterRestaurant from "./components/FilterRestaurant";
+import HomePageCuisine from "./components/HomePageCuisine";
+import Account from "./AccountComponents/Account";
+import Orders from "./AccountComponents/Orders";
+import Profile from "./AccountComponents/Profile";
+import PaymentCards from "./AccountComponents/PaymentCards";
+import AddressBook from "./AccountComponents/AddressBook";
+import NoonCredits from "./AccountComponents/NoonCredits";
+import { store } from "./app/store";
+
+
+
+/* const RestaurantDetails= lazy(()=> import("./components/RestaurantDetails")) */
 
 const AppLayout = () => {
+  const location=useLocation()
+ const [url,setUrl]=useState(location.pathname)
+  
+  useEffect(()=>{
+    setUrl(location.pathname)
+  },[location.pathname])
+ 
   return (
     <div className="app">
-      <Header />
-       <Outlet/>
+           <Provider store={store}>
+                    <CookiesProvider>
+                              <Header />
+                        <div className="flex">
+                             {                    !url.includes("/outlet") && !url.includes("/Account") &&(
+                                     <div className="w-[400px] px-[100px] m-6 bg-gray-100">
+                                          <NavSearchFacets />
+                                     </div>
+                              )
+                             }
+
+                                     <div>
+                                            <Outlet />
+                                    </div>
+                        </div>
+                    </CookiesProvider>
+             </Provider>
     </div>
   );
 };
 
-const appRouter=createBrowserRouter([
+const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<AppLayout/>,
-    errorElement:<Error />,
-    children:[
-      {
-        path:"/",
-        element:<Body/>
-      },
-      {
-        path:"/about",
-        element:<About/>
-      },
-      {
-        path:"/contact",
-        element:<Contact />
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [    
+       {  path: "/", element: <Body /> },
+       {  path: "/about", element: <About /> },
+       {  path: "/contact",element: <Contact />   },
+       {  path: "/outlet/:outletCode", element: <Card /> },
+       {  path: "/filterRestaurant", element: <FilterRestaurant /> },
+       {  path: "/homePageCuisine", element: <HomePageCuisine /> },
+       {
+        path: "/Account",
+        element: <Account />,
+        children:[
+          {  path:"Orders", element:<Orders />   },
+          {  path:"Profile", element:<Profile/>  },
+          {  path:"PaymentCards", element:<PaymentCards/>  },
+          {  path:"AddressBook", element:<AddressBook/>  },
+          {  path:"NoonCredits", element:<NoonCredits/>  },
+           ]
       }
-    ]
+    ],
   },
-  
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider  router={appRouter} />);
+root.render(<RouterProvider router={appRouter} />);
