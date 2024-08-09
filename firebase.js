5a3864404580851375de5564b0d73e869c0026ef
msa <mdsohailahmed117@gmail.com>
2024-08-09 05:20:12 +0400

    updatefirebase

--- firebase.js
+++ firebase.js
@@ -1,19 +1,22 @@
-// Import the functions you need from the SDKs you need
+
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration using environment variables
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyC56Aj71Lm9dsXo4ZcJGblGxsUr6OS0n1k",
+  authDomain: "noonproject-c76dc.firebaseapp.com",
+  projectId: "noonproject-c76dc",
+  storageBucket: "noonproject-c76dc.appspot.com",
+  messagingSenderId: "129848374950",
+  appId: "1:129848374950:web:aca86b86e9fbc016fba667"
 };
 
-// Initialize Firebase
+
+
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
