021e39a28b65b91edd600a15fdca5b9360ac91f9
msa <mdsohailahmed117@gmail.com>
2024-08-15 03:02:32 +0400

    update

--- src/components/ApiPrectice.js
+++ src/components/ApiPrectice.js
@@ -7,7 +7,7 @@ const ApiPrectice = () => {
   const fetchData = async() => {
     try {
         console.log("Fetching  data from API...");
-        const response = await axios.get(`${process.env.REACT_APP_API_URL || "http://localhost:8080"}/http://localhost:8080/https://food.noon.com/_svc/mp-food-api-catalog/api/`);
+        const response = await axios.get(`${process.env.VERCEL_URL}https://food.noon.com/_svc/mp-food-api-catalog/api/`);
         console.log("API Response: ", response);
         console.log("Response Data:", response.data);
         setData(response.data.results); // Adjust based on your data structure
@@ -18,6 +18,7 @@ const ApiPrectice = () => {
 
   useEffect(() => {
     fetchData();
+    console.log(process.env.REACT_APP_API_URL ,"vercel")
   }, []);
 
   return (
